4de870faa34fe764b20a57276178a2b64d9e8c6a
Mentor Graphics <mg30@ee215lnx05.ecn.purdue.edu>
2015-09-01 14:13:30 -0400

    initial add

---
+++ Lab2/source/adder_1bit.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   adder_1bit.sv
+// Created:     9/1/2015
+// Author:      Kyle Rakos
+// Lab Section: 337-01
+// Version:     1.0  Initial Design Entry
+// Description: 1 bit adder
