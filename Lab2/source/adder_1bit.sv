43bbf0363b6ba878afc256bfae2ccb1b972c8823
Mentor Graphics <mg30@ee215lnx05.ecn.purdue.edu>
2015-09-01 14:21:02 -0400

    Final commit

--- Lab2/source/adder_1bit.sv
+++ Lab2/source/adder_1bit.sv
@@ -5,3 +5,18 @@
 // Lab Section: 337-01
 // Version:     1.0  Initial Design Entry
 // Description: 1 bit adder
+
+module adder_1bit
+  (
+input wire a,
+   input wire b,
+   input wire carry_in
+   output wire sum
+   output wire carry_out
+   );
+
+   always @(a,b,carry_in) begin: COM
+      sum = carry_in ^ (a ^ b);
+      carry_out = ((!carry_in) && b && a) || (carry_in && (b || a))
+	end
+   end module
