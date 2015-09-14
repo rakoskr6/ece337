6e5795cd8bdafe8467c2e15d56426c2d6e6eb367
Mentor Graphics <mg30@ecegrid-thin5.ecn.purdue.edu>
2015-09-13 23:07:01 -0400

    initial commit

---
+++ Lab3/source/flex_pts_sr.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   flex_pts_sr.sv
+// Created:     9/13/2015
+// Author:      Kyle Rakos
+// Lab Section: 337-01
+// Version:     1.0  Initial Design Entry
+// Description: Flexible parallel to serial shift register
