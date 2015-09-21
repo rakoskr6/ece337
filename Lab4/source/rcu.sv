f860ccf29ba88e22084e92b067fe1fc90779df89
Mentor Graphics <mg30@ecegrid-thin5.ecn.purdue.edu>
2015-09-21 00:53:13 -0400

    Finished first itteration of files

--- Lab4/source/rcu.sv
+++ Lab4/source/rcu.sv
@@ -5,3 +5,78 @@
 // Lab Section: 337-01
 // Version:     1.0  Initial Design Entry
 // Description: receiver control unit
+
+module rcu
+  (
+   input clk,
+   input n_rst,
+   input start_bit_detected,
+   input packet_done,
+   input framing_error,
+   output sbc_clear,
+   output sbc_enable,
+   output load_buffer,
+   output enable_timer
+   );
+
+   typedef enum logic [2:0] {init, ReadData, Check, Receive, Load}
+   state_type;
+   state_type state, nextstate;
+
+   
+always_ff @(posedge clk, negedge n_rst)
+  begin
+     if (n_rst == 0)
+       begin
+	  state <= init;
+       end
+     else
+       begin
+	  state <= nextstate;
+       end
+  end // always_ff @ (posedge clk, negedge n_rst)
+
+   always_comb
+     begin
+	nextstate = state;
+	case(state)
+	  init:
+	    begin
+	       if (start_bit_detected == 1'b1)
+		 nextstate = ReadData;
+	       else
+		 nextstate = init;
+
+	    end
+
+	  ReadData:
+	    begin
+	       if (packet_done == 1'b1)
+		 nextstate = Check;
+	       else
+		 nextstate = ReadData;
+	    end
+	  Check:
+	    begin
+	       nextstate = Receive;
+	    end
+	  Receive:
+	    begin
+	       if (framing_error == 1'b1)
+		 nextstate = init;
+	       else
+		 nextstate = Load;
+	    end
+	endcase // case (state)
+     end // always_comb begin
+
+   // output logic
+   assign sbc_clear = (state == ReadData);
+   assign sbc_enable = (state == Check);
+   assign load_buffer = (state == Load);
+   assign enable_timer = (state == ReadData);
+     
+   
+endmodule // rcu
+
+   
--- Lab4/source/sr_9bit.sv
+++ Lab4/source/sr_9bit.sv
@@ -5,3 +5,22 @@
 // Lab Section: 337-01
 // Version:     1.0  Initial Design Entry
 // Description: 9 bit shift register
+
+module sr_9bit
+  (
+   input clk,
+   input n_rst,
+   input shift_strobe,
+   input serial_in,
+   output [7:0] packet_data,
+   output stop_bit
+   );
+
+   reg [8:0] all_data;
+
+   assign packet_data = all_data[7:0];
+   assign stop_bit = all_data[8];
+   
+   flex_stp_sr #(.NUM_BITS(8),.SHIFT_MSB(0)) DUT(.clk(clk), .n_rst(n_rst), .shift_enable(shift_strobe), .serial_in(serial_in), .parallel_out(all_data));
+
+endmodule // sr_9bit
