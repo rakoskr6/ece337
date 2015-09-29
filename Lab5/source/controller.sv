fc2f4034a0ced91a3bc6c5cb1a0cf5ed0d334d09
Mentor Graphics <mg30@ecegrid-thin4.ecn.purdue.edu>
2015-09-28 22:31:24 -0400

    finished first (working?) version of magnitude block. Started controller

--- Lab5/source/controller.sv
+++ Lab5/source/controller.sv
@@ -5,3 +5,131 @@
 // Lab Section: 337-01
 // Version:     1.0  Initial Design Entry
 // Description: regulates the controls the operation sequence of the entire system
+
+module controller
+  (
+   input wire clk,
+   input wire n_reset,
+   input wire dr,
+   input wire lc,
+   input wire overflow,
+   output reg cnt_up,
+   output reg clear,
+   output wire modwait,
+   output reg [2:0] op,
+   output reg [3:0] src1,
+   output reg [3:0] src2,
+   output reg [3:0] dest,
+   output reg err
+   );
+
+   typedef enum logic [3:0] {idle, store, zero, sort1, sort2, sort3, sort4,
+			     mul1, add1, mul2, sub1, mul3, add2, mul4, sub2,
+			     ,eidle} state_type;
+   state_type state, nextstate;
+   
+      
+always_ff @(posedge clk, negedge n_rst)
+  begin
+     if (n_rst == 0)
+       begin
+	  state <= init;
+
+       end
+     else
+       begin
+	  state <= nextstate;
+       end
+  end // always_ff @ (posedge clk, negedge n_rst)
+
+   always_comb
+     begin
+	nextstate = state;
+
+	case(state)
+	  init:
+	    begin
+
+	    end
+
+	  store:
+	    begin
+
+	    end
+
+	  zero:
+	    begin
+
+	    end
+
+	  sort1:
+	    begin
+
+	    end
+
+	  sort2:
+	    begin
+
+	    end
+
+	  sort3:
+	    begin
+
+	    end
+
+	  sort4:
+	    begin
+
+	    end
+
+	  mul1:
+	    begin
+
+	    end
+
+	  add1:
+	    begin
+
+	    end
+
+	  mul2:
+	    begin
+
+	    end
+
+	  sub1:
+	    begin
+
+	    end
+
+	  mul3:
+	    begin
+
+	    end
+
+	  add2:
+	    begin
+
+	    end
+
+	  mul4:
+	    begin
+
+	    end
+
+	  sub2:
+	    begin
+
+	    end
+
+	  eidle:
+	    begin
+
+	    end
+	  
+	end // always_comb begin
+   
+   
+   
+   
+endmodule
--- Lab5/source/magnitude.sv
+++ Lab5/source/magnitude.sv
@@ -5,3 +5,32 @@
 // Lab Section: 337-01
 // Version:     1.0  Initial Design Entry
 // Description: handles magnitude logic
+
+module magnitude
+  (
+   input wire [16:0] in,
+   output wire [15:0] out
+   );
+
+   reg [16:0] 	      temp;
+   
+
+   always_comb
+     begin
+	if (in < 0)
+	  begin
+	     temp = -in;
+	  end
+	else
+	  begin
+	     temp = in;
+	  end
+	
+	//out = temp[15:0];
+	
+     end
+
+   assign out = temp[15:0];
+   
+   
+endmodule
