264dced2ee242300b5c7348a2f3c60c5c2d1d28b
Mentor Graphics <mg30@ee215lnx04.ecn.purdue.edu>
2015-10-06 14:27:50 -0400

    finished code and first two test benches

--- Lab6/source/decode.sv
+++ Lab6/source/decode.sv
@@ -5,3 +5,62 @@
 // Lab Section: 337-01
 // Version:     1.0  Initial Design Entry
 // Description: decode block
+
+module decode
+  (
+   input clk,
+   input n_rst,
+   input d_plus,
+   input shift_enable,
+   input eop,
+   output d_orig
+   );
+   reg 	  current;
+   reg 	  prev;
+      
+      
+   always_ff @ (posedge clk, negedge n_rst)
+     begin
+	if (n_rst == 1'b0)
+	  begin
+	     current <= 1;
+	  end
+	else
+	  begin
+	     if (shift_enable == 1'b1)
+	       begin
+		  current <= d_plus;
+		  if (eop == 1'b1)
+		    begin
+		       current <= 1'b1;
+		    end
+		  current <= current;
+	       end
+	  end	
+     end // always_ff @
+   
+
+   always_ff @ (posedge clk, negedge n_rst)
+     begin
+	if (n_rst == 1'b0)
+	  begin
+	     prev <= 1;
+	  end
+	else
+	  begin
+	     if (shift_enable == 1'b1)
+	       begin
+		  prev <= current;
+		  if (eop == 1'b1)
+		    begin
+		       current <= 1'b1;
+		    end
+		  prev <= prev;
+	       end
+	  end	
+     end // always_ff @
+   
+   assign d_edge = current ^ prev;
+   
+
+endmodule
--- Lab6/source/rcv_fifo.sv
+++ Lab6/source/rcv_fifo.sv
@@ -5,3 +5,19 @@
 // Lab Section: 337-01
 // Version:     1.0  Initial Design Entry
 // Description: rcv fifo block
+
+module fifo
+  (
+   input clk,
+   input n_rst,
+   input r_enable,
+   input w_enable,
+   input [7:0] w_data,
+   output [7:0] r_data,
+   output empty,
+   output full
+   );
+
+   fifo(.r_clk(clk), .w_clk(clk), .n_rst(n_rst), .r_enable(r_enable), .w_enable(w_enable), .w_data(w_data), .r_data(r_data), .empty(empty), .full(full));
+   
+endmodule
