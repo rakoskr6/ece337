a03ca8d4cd0fd2a0973c4d3f46f5f88994290d47
Mentor Graphics <mg30@ecegrid-thin4.ecn.purdue.edu>
2015-10-20 03:26:31 -0400

    stuff compiles

--- Lab6/source/shift_register.sv
+++ Lab6/source/shift_register.sv
@@ -12,9 +12,9 @@ module shift_register
    input n_rst,
    input shift_enable,
    input d_orig,
-   output rcv_data[7:0]
+   output [7:0]rcv_data
    );
 
-   flx_stp_sr #(.NUM_BITS(8),.SHIFT_MSB(0)) FLXSTP(.clk(clk), .n_rst(n_rst), .shift_enable(shift_enable), .serial_in(d_orig), .parallel_out(rcv_data));
+   flex_stp_sr #(.NUM_BITS(8),.SHIFT_MSB(0)) FLXSTP(.clk(clk), .n_rst(n_rst), .shift_enable(shift_enable), .serial_in(d_orig), .parallel_out(rcv_data));
 
 endmodule
--- Lab6/source/timer.sv
+++ Lab6/source/timer.sv
@@ -16,13 +16,15 @@ module timer
    output byte_received
    );
 
-   reg 	  firstout;
+   reg 	  [3:0]firstout;
    reg 	  secondout;
+   reg [3:0] roll = 8;
+   
    
 
-   flex_counter #(.NUM_CNT_BITS(3)) FLX1(.clk(clk), .n_rst(n_rst), .clear(d_edge|| !rcving), .count_enable(rcving), .rollover_val(8), .count_out(firstout), .rollover_flag());
+   flex_counter #(.NUM_CNT_BITS(4)) FLX1(.clk(clk), .n_rst(n_rst), .clear(d_edge|| !rcving), .count_enable(rcving), .rollover_val(roll), .count_out(firstout), .rollover_flag());
 
-   flex_counter #(.NUM_CNT_BITS(3)) FLX2(.clk(clk), .n_rst(n_rst), .clear(byte_received || !rcving), .count_enable(firstout==2), .rollover_val(8), .count_out(), .rollover_flag(byte_received));
+   flex_counter #(.NUM_CNT_BITS(4)) FLX2(.clk(clk), .n_rst(n_rst), .clear(byte_received || !rcving), .count_enable(firstout==2), .rollover_val(roll), .count_out(), .rollover_flag(byte_received));
 
    assign shift_enable = (firstout == 2);
 
